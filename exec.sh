9850f58228e4fb006437a4fd1ba1fefabcee9b0f
iqbalkhan82 <120130102+iqbalkhan82@users.noreply.github.com>
2022-12-25 21:42:31 +0500

    Update exec.sh

--- exec.sh
+++ exec.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-wget https://github.com/iqbalkhan82/PresentationPrep/blob/main/Dockerfile
+wget https://raw.githubusercontent.com/iqbalkhan82/PresentationPrep/main/Dockerfile
 docker build -t prep:latest .
 docker run -d --name PrepTest -p 8081:80 localhost/prep
 # Run Following command to make this file executable
 #chmod u+x exec.sh
+#https://raw.githubusercontent.com/iqbalkhan82/PresentationPrep/main/Dockerfile
+#https://github.com/iqbalkhan82/PresentationPrep/blob/main/Dockerfile
